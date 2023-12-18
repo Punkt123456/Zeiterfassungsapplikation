7d15e823d53f9025afe68e0cb9a9a383f5dfa95b
Hussein Kronboch <hussein.kronboch1@gmail.com>
2023-12-18 15:30:18 +0100

    Integrierung Registrierung

--- main.py
+++ main.py
@@ -12,17 +12,19 @@ db = SQLAlchemy(app)
 # Datenbank-Modell für Benutzer erstellen
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)  # corrected column name
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    name = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(15))  # optional
 
 # Beispielbenutzer in die Datenbank einfügen (ersetzen Sie dies durch Ihre eigene Logik)
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
 
-        if not User.query.filter_by(username='admin').first():
-            hashed_password = generate_password_hash('admin_password', method='sha256')
-            admin_user = User(username='admin', password_hash=hashed_password)
+        if not User.query.filter_by(email='admin@example.com').first():
+            hashed_password = generate_password_hash('admin_password', method='pbkdf2:sha256')
+            admin_user = User(email='admin@example.com', password_hash=hashed_password, name='Admin')
             db.session.add(admin_user)
             db.session.commit()
 
@@ -34,15 +36,40 @@ def index():
 # Route für den Login
 @app.route('/login', methods=['POST'])
 def login():
-    username = request.form.get('username')
+    email = request.form.get('email')
     password = request.form.get('password')
 
-    user = User.query.filter_by(username=username).first()
+    user = User.query.filter_by(email=email).first()
 
     if user and check_password_hash(user.password_hash, password):
-        return redirect(url_for('dashboard', username=username))
+        return redirect(url_for('dashboard', email=email))
     else:
-        return 'Falscher Benutzername oder Passwort'
+        return 'Falsche E-Mail oder Passwort'
+
+# Route für die Registrierungsseite
+@app.route('/registrierung')
+def registrierung():
+    return render_template('Registrierung.html')
+
+# Route zum Verarbeiten des Registrierungsformulars
+@app.route('/register', methods=['POST'])
+def register():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    name = request.form.get('name')
+    phone = request.form.get('phone')
+
+    # Überprüfe, ob die E-Mail bereits vorhanden ist
+    if User.query.filter_by(email=email).first():
+        return 'E-Mail bereits registriert'
+
+    # Hash des Passworts erstellen und Benutzer in die Datenbank einfügen
+    hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
+    new_user = User(email=email, password_hash=hashed_password, name=name, phone=phone)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return 'Registrierung erfolgreich'
 
 if __name__ == '__main__':
     app.run(debug=True)
